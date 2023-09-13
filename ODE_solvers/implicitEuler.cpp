6237d37d894721d24c8697958d79bff53fe796c6
guudie <gottychaos@gmail.com>
2023-09-13 23:03:03 +0700

    added option to separate the calculation of y and z

--- ODE_solvers/implicitEuler.cpp
+++ ODE_solvers/implicitEuler.cpp
@@ -10,3 +10,11 @@ void implicitEuler::integrate(glm::vec2& y, glm::vec2& z, glm::vec2 zdash, float
     z += g_func(t, y, z, zdash) * dt;
     y += z * dt;
 }
+
+void implicitEuler::integrateStep1(glm::vec2& y, glm::vec2& z, glm::vec2 zdash, float dt, float t) {
+    z += g_func(t, y, z, zdash) * dt;
+}
+
+void implicitEuler::integrateStep2(glm::vec2& y, glm::vec2 z, float dt, float t = 0) {
+    y += z * dt;
+}
--- ODE_solvers/implicitEuler.h
+++ ODE_solvers/implicitEuler.h
@@ -5,4 +5,8 @@ class implicitEuler : ODESolver {
 public:
     implicitEuler(utilFunc g);
     void integrate(glm::vec2& y, glm::vec2& z, glm::vec2 zdash, float dt, float t = 0) override;
+
+    // for separating the calculation of z and y when iterating through all particles
+    void integrateStep1(glm::vec2& y, glm::vec2& z, glm::vec2 zdash, float dt, float t = 0);
+    void integrateStep2(glm::vec2& y, glm::vec2 z, float dt, float t = 0);
 };
