9ee6724f4323aca93e64073cc87c31c54137fab1
guudie <gottychaos@gmail.com>
2023-10-02 23:06:41 +0700

    cleaned up code and added global var

--- main.cpp
+++ main.cpp
@@ -9,26 +9,16 @@
 #include "mouse.h"
 #include "utils.h"
 #include "fluid_sim.h"
+#include "global.h"
+
+const char* generalConfigPath = "config/general.cfg";
+const char* utilsConfigPath = "config/utils.cfg";
 
 int main() {
     const int width = 512, height = 512;
-    std::string configPath = "config/general.cfg";
-    std::string utilsConfigPath = "config/utils.cfg";
 
     libconfig::Config cfg;
-    try {
-        cfg.readFile(configPath);
-    }
-    catch(const libconfig::FileIOException &fioex)
-    {
-        std::cerr << "I/O error while reading file." << std::endl;
-        return EXIT_FAILURE;
-    }
-    catch(const libconfig::ParseException &pex)
-    {
-        std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine() << " - " << pex.getError() << std::endl;
-        return EXIT_FAILURE;
-    }
+    parseConfig(cfg, generalConfigPath);
 
     float dt = cfg.lookup("dt");
     glm::vec2 G;
@@ -41,27 +31,13 @@ int main() {
 
     fluid_sim* sim = new fluid_sim();
     sim->setup(cfg, width, height, &_integrator);
-
-    glm::ivec2 tl(0, 450);
-    glm::ivec2 br(width-1, height-11);
-    float dist = sim->getH() - 0.0001f;
-
-    sim->generateParticles(tl, br, dist);
-
-    tl = { 200, 100 };
-    br = { 350, 175 };
-    int cnt = 0;
+    sim->generateInitialParticles();
 
     Uint32 lastUpd = SDL_GetTicks();
     while(sim->isRunning()) {
         Uint32 curTime = SDL_GetTicks();
         if(curTime - lastUpd >= 16) {
             sim->input();
-            if(sim->getMouseObject()->getRB() && cnt < 6) {
-                sim->generateParticles(tl, br, sim->getH());
-                sim->getMouseObject()->setRB(false);
-                cnt++;
-            }
 
             try {
                 sim->update();
