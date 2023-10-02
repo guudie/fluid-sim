26c3ee59e5e24b60995cc9dcb2c57ddb8c8a86a8
guudie <gottychaos@gmail.com>
2023-10-02 20:38:19 +0700

    moved definitions to source

--- mouse.cpp
+++ mouse.cpp
@@ -1,5 +1,17 @@
 #include "mouse.h"
 
+const glm::vec2& mouse::getPos() const {
+    return pos;
+}
+
+const glm::vec2& mouse::getPrevPos() const {
+    return prevPos;
+}
+
+glm::vec2 mouse::getDiff() const {
+    return pos - prevPos;
+}
+
 void mouse::updatePos(const glm::vec2& _pos) {
     prevPos = pos;
     pos = _pos;
@@ -9,4 +21,28 @@ void mouse::updatePos(int x, int y) {
     prevPos = pos;
     pos.x = x;
     pos.y = y;
+}
+
+bool mouse::getLB() const {
+    return leftButtonDown;
+}
+
+void mouse::setLB(bool state) {
+    leftButtonDown = state;
+}
+
+bool mouse::getRB() const {
+    return rightButtonDown;
+}
+
+void mouse::setRB(bool state) {
+    rightButtonDown = state;
+}
+
+bool mouse::getSBX2() const {
+    return sideButtonX2Down;
+}
+
+void mouse::setSBX2(bool state) {
+    sideButtonX2Down = state;
 }
--- mouse.h
+++ mouse.h
@@ -14,18 +14,18 @@ public:
     mouse() : pos({0, 0}), prevPos({0, 0}), leftButtonDown(false), rightButtonDown(false), sideButtonX2Down(false) { }
     ~mouse() = default;
 
-    const glm::vec2& getPos() const { return pos; }
-    const glm::vec2& getPrevPos() const { return prevPos; }
-    glm::vec2 getDiff() const { return pos - prevPos; }
+    const glm::vec2& getPos() const;
+    const glm::vec2& getPrevPos() const;
+    glm::vec2 getDiff() const;
     void updatePos(const glm::vec2& _pos);
     void updatePos(int x, int y);
 
-    bool getLB() const { return leftButtonDown; }
-    void setLB(bool state) { leftButtonDown = state; }
+    bool getLB() const;
+    void setLB(bool state);
 
-    bool getRB() const { return rightButtonDown; }
-    void setRB(bool state) { rightButtonDown = state; }
+    bool getRB() const;
+    void setRB(bool state);
 
-    bool getSBX2() const { return sideButtonX2Down; }
-    void setSBX2(bool state) { sideButtonX2Down = state; }
+    bool getSBX2() const;
+    void setSBX2(bool state);
 };
